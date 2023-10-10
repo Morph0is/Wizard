24178178527f7bfa079d2b15aadc9f51173d18eb
Tauwi93 <soeun.thawrit@bene-edu.ch>
2023-10-10 20:01:04 +0200

    WIZARD * TEST

--- src/main/java/com/example/wizard/controller/WizardController.java
+++ src/main/java/com/example/wizard/controller/WizardController.java
@@ -118,7 +118,9 @@ public class WizardController {
     }
 
 
-    //Neue Person einfügen
+    /**
+     * Methode um eine neue Person zu erstellen
+     */
     public void insertPerson() {
 
         try {
